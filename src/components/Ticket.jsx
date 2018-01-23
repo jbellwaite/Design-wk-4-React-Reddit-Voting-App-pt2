47d744548bc8b62ceb6dda664ad609cf9e77745c
Jessie Waite and Susannah Lowe <jessie+susannah@>
2018-01-23 11:10:04 -0800

    Add functionaliry to upvote or downvote queue tickets, and display the total number of votes a queue ticket has.

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,20 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Ticket(props){
-  return (
-    <div>
-      <h3>{props.location} - {props.names}</h3>
-      <p><em>{props.issue}</em></p>
-      <hr/>
-    </div>
-  );
+class Ticket extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      voteTotal: 0
+    };
+    this.handleClickUp = this.handleClickUp.bind(this);
+    this.handleClickDown = this.handleClickDown.bind(this);
+  }
+
+  handleClickUp(){
+    let newVoteUp = this.state.voteTotal+=1;
+    this.setState({voteTotal: newVoteUp});
+  }
+
+  handleClickDown(){
+    let newVoteDown = this.state.voteTotal-=1;
+    this.setState({voteTotal: newVoteDown});
+  }
+
+  render(){
+    return (
+      <div>
+        <h3>{this.props.location} - {this.props.names}</h3>
+        <p><em>{this.props.issue}</em></p>
+        <button onClick={this.handleClickUp}>UpVote</button><span>   </span><button onClick={this.handleClickDown}>DownVote</button>
+        <p>{this.state.voteTotal}</p>
+        <hr/>
+      </div>
+    );
+  }
 }
 
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  issue: PropTypes.string
+  issue: PropTypes.string,
 };
 
 export default Ticket;
